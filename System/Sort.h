efce98d232c9c26f390074c154b348a5961cb30c
Oskar Viberg <bumblebullet@gmail.com>
2017-01-13 00:15:21 +0100

    Quicksort
    Sort functions made static

--- System/Sort.h
+++ System/Sort.h
@@ -3,7 +3,38 @@
 #include <vector>
 
 template <typename T>
-void sort_merge(vector<T>& v){
+static void sort_quick(vector<T>& v, const size_t& low = 0, size_t high = 0){
+
+	static auto partition = [](vector<T>& v, const size_t& low, const size_t& high){
+		size_t p = high - 1;
+
+		if(p - low == 1){
+			if(v[low] > v[p]) swap(v[low], v[p]);
+			return p;
+		}
+
+		for(size_t i = low; i < p;){
+			if(v[i] > v[p]){
+				swap(v[p], v[--p]);
+				if(i < p) swap(v[p + 1], v[i]);
+			}
+			else ++i;
+		}
+		return p;
+	};
+
+	if(!high) high = v.size();
+
+	if(high - low < 2) return;
+
+	size_t p = partition(v, low, high);
+
+	quicksort(v, low, p);
+	quicksort(v, p, high);
+}
+
+template <typename T>
+static void sort_merge(vector<T>& v){
 	size_t s = v.size();
 
 	if(s < 2) return;
@@ -25,7 +56,7 @@ void sort_merge(vector<T>& v){
 }
 
 template <typename T>
-void sort_insertion(std::vector<T>& vec){
+static void sort_insertion(std::vector<T>& vec){
 	for(size_t i = 1; i < vec.size(); ++i){
 		for(size_t j = i; j > 0 && vec[j] < vec[j - 1]; --j){
 			std::swap(vec[j], vec[j - 1]);
@@ -34,7 +65,7 @@ void sort_insertion(std::vector<T>& vec){
 }
 
 template <typename T>
-void sort_bubble(std::vector<T>& vec){
+static void sort_bubble(std::vector<T>& vec){
 	size_t end = vec.size();
 
 	bool swapped;
